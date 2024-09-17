d1fb5343200aabfb8e963ebf04c590976fff725b
heloez <escolaanaehelo@gmail.com>
2024-09-17 10:48:24 -0300

    Paramos no registrovm

---
+++ GcookA/Helpers/Helper.cs
@@ -0,0 +1,20 @@
+using System.Net.Mail;
+
+namespace GcookA.Helpers;
+
+public static class Helper
+{
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                MailAddress m = new(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+
+        }
+}
